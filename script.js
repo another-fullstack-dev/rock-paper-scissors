19465c4de7ffdb134b90a324ce5b44a43d214b40
draemato <97753719+draemato@users.noreply.github.com>
2024-04-15 12:25:22 +0700

    Define playRound function

--- script.js
+++ script.js
@@ -5,15 +5,44 @@ function getComputerChoice(min, max){
     choiceNumber = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled)
     switch (choiceNumber) {
         case 0:
-            choice = "Rock";
+            choice = "rock";
             break;
     
         case 1:
-            choice = "Paper";
+            choice = "paper";
             break;
 
         case 2:
-            choice = "Scissors";
+            choice = "scissors";
     }
     return choice;
+}
+
+getComputerChoice(0, 2);
+
+function playRound(playerChoice, computerChoice = getComputerChoice(0, 2)){
+    playerChoice.toLowerCase();
+    let result;
+
+    if(playerChoice === "rock" && computerChoice === "scissors"){
+        return result = "You won.";
+    } else if(playerChoice === "rock" && computerChoice === "paper"){
+        return result = "You lost.";
+    }
+    
+    if(playerChoice === "paper" && computerChoice === "rock"){
+        return result = "You won.";
+    } else if(playerChoice === "paper" && computerChoice === "scissors"){
+       return result = "You lost.";
+    }
+
+    if(playerChoice === "scissors" && computerChoice === "paper"){
+        return result = "You won.";
+    } else if(playerChoice === "scissors" && computerChoice === "rock"){
+       return result = "You lost.";
+    }
+
+    if(playerChoice === computerChoice) {
+        return result = "Thats a draw.";
+    }
 }
