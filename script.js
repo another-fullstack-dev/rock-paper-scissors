6fbd898eed07c276c174ef76bd6daa6c0f604cca
draemato <97753719+draemato@users.noreply.github.com>
2024-04-15 17:31:20 +0700

    Defined final functions
    Theres a bunch of stuff that changed really and i probably shoudve
    commited earlier but who cares.
    
    Current defenitions are sloppy af and probably just terrible, frankly.

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+let choice;
+let playerSelection;
+let result;
+
 function getComputerChoice(min, max){
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
-    let choice;
     choiceNumber = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled)
     switch (choiceNumber) {
         case 0:
@@ -20,10 +23,18 @@ function getComputerChoice(min, max){
 
 getComputerChoice(0, 2);
 
-function playRound(playerChoice, computerChoice = getComputerChoice(0, 2)){
-    playerChoice.toLowerCase();
-    let result;
+function getPlayerChoice(){
+    playerSelection = prompt("Choose: Rock, Paper, or Scissors?");
+    playerSelection = playerSelection.toLowerCase();
+    return playerSelection;
+    }
 
+playerSelection = "paper";
+
+function playRound(playerChoice = playerSelection, computerChoice = choice){
+    getPlayerChoice();
+
+    playerSelection = playerSelection.toLowerCase();
     if(playerChoice === "rock" && computerChoice === "scissors"){
         return result = "You won.";
     } else if(playerChoice === "rock" && computerChoice === "paper"){
@@ -44,5 +55,30 @@ function playRound(playerChoice, computerChoice = getComputerChoice(0, 2)){
 
     if(playerChoice === computerChoice) {
         return result = "Thats a draw.";
+    }
+}
+
+function playGame(){
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) { // i should keep i mind that indexing begins from zero next time
+        playRound();
+        if(result === "You won.") {
+            ++playerScore;
+            console.log(`You have won the round. Your score is ${playerScore} against ${computerScore}`);
+        } else if(result === "You lost."){
+            ++computerScore;
+            console.log(`You have lost the round. Your score is ${playerScore} against ${computerScore}`);
+        } else if(result === "Thats a draw."){
+            console.log(`This is a draw. Scores remain unchanged. Your score is ${playerScore} against ${computerScore} `)
+        }
+
+        if(playerScore > computerScore && i == 4){
+            console.log(`You have won the game! Your score was ${playerScore} against ${computerScore}!`);
+        } else if(playerScore < computerScore && i == 4){
+            console.log(`You have lost the game! Your score was ${playerScore} against ${computerScore}!`);
+        } else if(playerScore === computerScore && i == 4){
+            console.log(`Game ended with a draw! Your score was ${playerScore} against ${computerScore}!`)
+        }
     }
 }
